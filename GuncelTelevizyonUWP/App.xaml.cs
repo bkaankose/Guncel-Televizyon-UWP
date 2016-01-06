2f347e2c93189fff21961cad8b2916b575b93126
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-06 23:38:07 +0200

    Unity container and ViewModelFactory are implemented.

--- GuncelTelevizyonUWP/App.xaml.cs
+++ GuncelTelevizyonUWP/App.xaml.cs
@@ -7,6 +7,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
+using Microsoft.Practices.Unity;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -17,20 +18,67 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using System.Threading.Tasks;
 using GuncelTelevizyonUWP.Views;
+using Prism.Windows.Mvvm;
+using GuncelTelevizyonUWP.Interfaces;
+using GuncelTelevizyonUWP.Repositories;
+using GuncelTelevizyonUWP.Services;
+using System.Reflection;
+using GuncelTelevizyonUWP.Helpers;
+using Prism.Mvvm;
 
 namespace GuncelTelevizyonUWP
 {
     sealed partial class App : PrismApplication
     {
+        public T Resolve<T>() => _container.Resolve<T>();
+        private Assembly[] cachedAssemblies = null;
+        private Dictionary<string, Type> cachedViewModels = null;
+        private IUnityContainer _container { get; }
         public App()
         {
             this.InitializeComponent();
-            //this.Suspending += OnSuspending;
+            _container = new UnityContainer();
         }
 
         protected override async Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
         {
+            await RegisterRepositories();
             NavigationService.Navigate("Main", null);
+        }
+
+        private Type PinpointViewModel(Type viewType)
+        {
+            var viewModelProperty = viewType.GetRuntimeProperty("ViewModel");
+            if (viewModelProperty != null)
+                return viewModelProperty.PropertyType;
+
+            var tn = viewType.Name + "ViewModel";
+
+            if (cachedViewModels != null && cachedViewModels.ContainsKey(tn))
+                return cachedViewModels[tn];
+
+            if (cachedAssemblies == null)
+            {
+                cachedAssemblies = ReflectionHelper.GetAssemblies().ToArray();
+            }
+
+            foreach (var a in cachedAssemblies)
+            {
+                var vm =
+                    a.GetType($"{a.GetName().Name}.ViewModels.{tn}")
+                    ?? a.GetType($"Tcm.Sfa.ViewModels.{tn}"); // hack
+
+                if (vm != null)
+                {
+                    if (cachedViewModels == null) cachedViewModels = new Dictionary<string, Type>();
+                    if (!cachedViewModels.ContainsKey(tn))
+                        cachedViewModels.Add(tn, vm);
+
+                    return vm;
+                }
+            }
+
+            return null;
         }
         /// <summary>
         /// Invoked when Navigation to a certain page fails
@@ -49,6 +97,22 @@ namespace GuncelTelevizyonUWP
         /// </summary>
         /// <param name="sender">The source of the suspend request.</param>
         /// <param name="e">Details about the suspend request.</param>
+        /// 
+
+        private object ViewModelFactoy(Type modelType)
+        {
+            return _container.Resolve(modelType);
+        }
+        private async Task RegisterRepositories()
+        {
+            ViewModelLocationProvider.SetDefaultViewModelFactory(ViewModelFactoy);
+            ViewModelLocationProvider.SetDefaultViewTypeToViewModelTypeResolver(PinpointViewModel);
+
+            _container.RegisterInstance<ISettingsService>(Resolve<SettingsService>());
+            _container.RegisterInstance<ISettingsRepository>(Resolve<SettingsRepository>());
+            //var a = Resolve<SettingsRepository>();
+
+        }
         private void OnSuspending(object sender, SuspendingEventArgs e)
         {
             var deferral = e.SuspendingOperation.GetDeferral();
