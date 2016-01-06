ae57bd1d07fbd435f883357c8957764895c2d0fb
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-06 22:29:18 +0200

    Empty SettingsRepository and ChannelRepository implemented.

---
+++ GuncelTelevizyonUWP/Repositories/SettingsRepository.cs
@@ -0,0 +1,14 @@
+﻿using GuncelTelevizyonUWP.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuncelTelevizyonUWP.Repositories
+{
+    public class SettingsRepository : ISettingsRepository
+    {
+
+    }
+}
