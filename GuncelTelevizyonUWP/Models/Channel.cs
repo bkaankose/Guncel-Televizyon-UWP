3e4181f0b96436448d6089b66c4de1e955e122b9
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-06 22:35:45 +0200

    Basic channel models are implemented.

---
+++ GuncelTelevizyonUWP/Models/Channel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuncelTelevizyonUWP.Models
+{
+    public class Channel
+    {
+        public int Id { get; set; }
+        public int StreamInformationId { get; set; }
+        public string Name { get; set; }
+        public string StreamUrl { get; set; }
+        public bool IsPrivate { get; set; }
+        
+    }
+}
