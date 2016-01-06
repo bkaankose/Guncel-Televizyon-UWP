697bba258293587bb1d7e7af83b33e6f614fb3c4
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-06 23:37:36 +0200

    SettingsService's interface methods are implemented for test purposes.

---
+++ GuncelTelevizyonUWP/Services/SettingsService.cs
@@ -0,0 +1,27 @@
+﻿using GuncelTelevizyonUWP.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GuncelTelevizyonUWP.Models;
+using Windows.Storage;
+
+namespace GuncelTelevizyonUWP.Services
+{
+    public class SettingsService : ISettingsService
+    {
+        public async Task<Settings> GetSettingsAsync()
+        {
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            StorageFile settingsJson = await storageFolder.GetFileAsync("settings.json");
+
+            return null;
+        }
+
+        public Task<bool> SaveSettingsAsync(Settings _settingsModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
