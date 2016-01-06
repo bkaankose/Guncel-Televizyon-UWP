f079fa6fc6c4ea4618426cfafd169fe3b46b5522
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-06 22:35:59 +0200

    Namespace correction for Repositories.

--- GuncelTelevizyonUWP/Interfaces/IChannelRepository.cs
+++ GuncelTelevizyonUWP/Interfaces/IChannelRepository.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GuncelTelevizyonUWP.Repositories
+namespace GuncelTelevizyonUWP.Interfaces
 {
-    class IChannelRepository
+    public interface IChannelRepository
     {
     }
 }
--- GuncelTelevizyonUWP/Interfaces/ISettingsRepository.cs
+++ GuncelTelevizyonUWP/Interfaces/ISettingsRepository.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GuncelTelevizyonUWP.Repositories
+namespace GuncelTelevizyonUWP.Interfaces
 {
     public interface ISettingsRepository
     {
