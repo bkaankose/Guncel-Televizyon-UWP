ea8d60fd0e1dbf2c8f254cdf9e71d934d2a97ffe
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-06 23:36:34 +0200

    SettingsRepository modified.
    Basic methods are evolved.

--- GuncelTelevizyonUWP/Repositories/SettingsRepository.cs
+++ GuncelTelevizyonUWP/Repositories/SettingsRepository.cs
@@ -4,11 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GuncelTelevizyonUWP.Models;
+using GuncelTelevizyonUWP.Services;
 
 namespace GuncelTelevizyonUWP.Repositories
 {
     public class SettingsRepository : ISettingsRepository
     {
+        private ISettingsService _settingsService;
+        public SettingsRepository(ISettingsService settingsService)
+        {
+            _settingsService = settingsService;
+        }
 
+        public async Task<Settings> GetSettingsAsync()
+        {
+            return await _settingsService.GetSettingsAsync();
+        }
+
+        public async Task<bool> SaveSettingsAsync(Settings _settingsModel)
+        {
+            return await _settingsService.SaveSettingsAsync(_settingsModel);
+        }
     }
 }
