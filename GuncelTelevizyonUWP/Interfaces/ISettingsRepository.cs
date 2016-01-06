b6796415d373eb27c63b75c1a67a017cac4b8915
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-06 23:37:06 +0200

    ISettingsService updated.

--- GuncelTelevizyonUWP/Interfaces/ISettingsRepository.cs
+++ GuncelTelevizyonUWP/Interfaces/ISettingsRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GuncelTelevizyonUWP.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace GuncelTelevizyonUWP.Interfaces
 {
     public interface ISettingsRepository
     {
-
+        Task<Settings> GetSettingsAsync();
+        Task<bool> SaveSettingsAsync(Settings _settingsModel);
     }
 }
