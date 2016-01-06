50ad19855a5e9d11578e4f5c4ab45760d4ac2531
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-06 23:38:22 +0200

    MainPageViewModel is updated for test purposes.

--- GuncelTelevizyonUWP/ViewModels/MainPageViewModel.cs
+++ GuncelTelevizyonUWP/ViewModels/MainPageViewModel.cs
@@ -1,17 +1,31 @@
-﻿using Prism.Windows.Mvvm;
+﻿using GuncelTelevizyonUWP.Interfaces;
+using Prism.Windows.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Prism.Windows.Navigation;
 
 namespace GuncelTelevizyonUWP.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        public MainPageViewModel()
-        {
+        #region Repositories
 
+        private ISettingsRepository _settingsRepository;
+
+        #endregion
+
+        public MainPageViewModel(ISettingsRepository settingsRepository)
+        {
+            _settingsRepository = settingsRepository;
+        }
+
+        public override async void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+        {
+            base.OnNavigatedTo(e, viewModelState);
+            var a = await _settingsRepository.GetSettingsAsync();
         }
     }
 }
