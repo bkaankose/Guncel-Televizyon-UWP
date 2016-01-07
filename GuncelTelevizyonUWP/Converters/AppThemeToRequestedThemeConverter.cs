ffeb054f320d81d81f2045d7ca370b48c62904c6
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-07 03:22:02 +0200

    Requested Theme converter.

---
+++ GuncelTelevizyonUWP/Converters/AppThemeToRequestedThemeConverter.cs
@@ -0,0 +1,28 @@
+﻿using GuncelTelevizyonUWP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace GuncelTelevizyonUWP.Converters
+{
+    public class AppThemeToRequestedThemeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var sent = (AppTheme)value;
+            if (sent == AppTheme.Dark)
+                return ElementTheme.Dark;
+            else
+                return ElementTheme.Light;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
