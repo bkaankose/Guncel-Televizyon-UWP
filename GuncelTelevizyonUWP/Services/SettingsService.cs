ad24af7be64b7b086e1e66f2d7a6a9686a1e3d86
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-07 03:16:42 +0200

    GetSettings method modified.

--- GuncelTelevizyonUWP/Services/SettingsService.cs
+++ GuncelTelevizyonUWP/Services/SettingsService.cs
@@ -6,6 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using GuncelTelevizyonUWP.Models;
 using Windows.Storage;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace GuncelTelevizyonUWP.Services
 {
@@ -14,9 +17,12 @@ namespace GuncelTelevizyonUWP.Services
         public async Task<Settings> GetSettingsAsync()
         {
             StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
-            StorageFile settingsJson = await storageFolder.GetFileAsync("settings.json");
-
-            return null;
+            if (File.Exists(string.Format("{0}\\{1}", storageFolder.Path, "settings.json")))
+            {
+                StorageFile settingsJson = await storageFolder.GetFileAsync("settings.json");
+                return JsonConvert.DeserializeObject<Settings>((JObject.Parse(await FileIO.ReadTextAsync(settingsJson)).ToString()));
+            }else
+                return new Settings() { Theme = AppTheme.Dark, FavoritedChannelIds = new List<int>() };
         }
 
         public Task<bool> SaveSettingsAsync(Settings _settingsModel)
