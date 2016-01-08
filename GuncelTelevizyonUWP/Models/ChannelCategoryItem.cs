71b6cbff16412b7a01ca6836d28f8d48c7fada14
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-08 12:55:33 +0200

    HamburgerMenuItem renamed as ChannelCategoryItem

---
+++ GuncelTelevizyonUWP/Models/ChannelCategoryItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuncelTelevizyonUWP.Models
+{
+    public class ChannelCategoryItem
+    {
+        public string Icon { get; set; }
+        public string Title { get; set; }
+    }
+}
