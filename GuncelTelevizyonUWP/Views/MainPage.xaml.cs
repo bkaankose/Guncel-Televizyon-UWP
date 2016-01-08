2e28319e33a1526af5a7afc6c2171939db8fd27a
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-08 23:34:13 +0200

    New Master-Detail implementation for MainPage contents.

--- GuncelTelevizyonUWP/Views/MainPage.xaml.cs
+++ GuncelTelevizyonUWP/Views/MainPage.xaml.cs
@@ -1,13 +1,23 @@
 ﻿using GuncelTelevizyonUWP.Helpers;
+using GuncelTelevizyonUWP.ViewModels;
 using Prism.Windows.Mvvm;
 
 namespace GuncelTelevizyonUWP.Views
 {
     public sealed partial class MainPage : BasePage
     {
+        private MainPageViewModel MainVM;
         public MainPage()
         {
             this.InitializeComponent();
+            MainVM = this.DataContext as MainPageViewModel;
+            MainVM.PropertyChanged += VMPropertyChanged;
+        }
+
+        private void VMPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == "SubPageChanged")
+                subView.SetContent();
         }
     }
 }
