20cd8d717c0c9d56b1f45be3cfa9e1279e48c85d
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-08 22:18:22 +0200

    Basic SettingsPage implemented.

---
+++ GuncelTelevizyonUWP/ViewModels/SettingsPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using GuncelTelevizyonUWP.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuncelTelevizyonUWP.ViewModels
+{
+    public class SettingsPageViewModel : BaseViewModel
+    {
+        public SettingsPageViewModel()
+        {
+
+        }
+    }
+}
