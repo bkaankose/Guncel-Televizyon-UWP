2fb10410b7b139205f3472a712096a7bf315f649
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-08 12:42:13 +0200

    MainPage modified.

--- GuncelTelevizyonUWP/Views/MainPage.xaml.cs
+++ GuncelTelevizyonUWP/Views/MainPage.xaml.cs
@@ -1,22 +1,9 @@
-﻿using Prism.Windows.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+﻿using GuncelTelevizyonUWP.Helpers;
+using Prism.Windows.Mvvm;
 
 namespace GuncelTelevizyonUWP.Views
 {
-    public sealed partial class MainPage : SessionStateAwarePage
+    public sealed partial class MainPage : BasePage
     {
         public MainPage()
         {
