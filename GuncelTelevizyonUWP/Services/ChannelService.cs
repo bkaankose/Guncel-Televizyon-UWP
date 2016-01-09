c9e8cfc52c745dd12c96ffd2fd6eee67f99fabb7
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-10 00:57:33 +0200

    Empty ChannelService implementation.

---
+++ GuncelTelevizyonUWP/Services/ChannelService.cs
@@ -0,0 +1,14 @@
+﻿using GuncelTelevizyonUWP.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuncelTelevizyonUWP.Services
+{
+    public class ChannelService : IChannelService
+    {
+
+    }
+}
