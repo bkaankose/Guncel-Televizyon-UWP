80e24d6b355c1ca81dc630a4bfcfcac653296174
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-09 19:59:03 +0200

    2 new theme converters implemented in order to adapt the new SettingsPage UI.

---
+++ GuncelTelevizyonUWP/Converters/DarkAppThemeToBooleanConverter.cs
@@ -0,0 +1,31 @@
+﻿using GuncelTelevizyonUWP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace GuncelTelevizyonUWP.Converters
+{
+    public class DarkAppThemeToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) // Unexpected
+                return false;
+
+            var sentValue = (AppTheme)value;
+            return sentValue == AppTheme.Dark ? true : false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) // Unexpected
+                return false;
+
+            var sentValue = (bool)value;
+            return sentValue == true ? AppTheme.Dark : AppTheme.Light;
+        }
+    }
+}
---
+++ GuncelTelevizyonUWP/Converters/LightThemeToBooleanConverter.cs
@@ -0,0 +1,31 @@
+﻿using GuncelTelevizyonUWP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace GuncelTelevizyonUWP.Converters
+{
+    public class LightThemeToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) // Unexpected
+                return false;
+
+            var sentValue = (AppTheme)value;
+            return sentValue == AppTheme.Light ? true : false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) // Unexpected
+                return false;
+
+            var sentValue = (bool)value;
+            return sentValue == true ? AppTheme.Light : AppTheme.Dark;
+        }
+    }
+}
