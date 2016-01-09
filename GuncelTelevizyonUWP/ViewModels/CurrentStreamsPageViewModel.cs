b2daa6deffaeb454743b143e232c259afe36f06d
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-10 00:58:07 +0200

    Empty new Current Streams Page implemented.

---
+++ GuncelTelevizyonUWP/ViewModels/CurrentStreamsPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using GuncelTelevizyonUWP.Helpers;
+using GuncelTelevizyonUWP.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuncelTelevizyonUWP.ViewModels
+{
+    public class CurrentStreamsPageViewModel : BaseViewModel
+    {
+        #region Repositories
+        private readonly IChannelRepository _channelRepository;
+
+        #endregion
+        public CurrentStreamsPageViewModel(IChannelRepository channelRepository)
+        {
+            _channelRepository = channelRepository;
+        }
+    }
+}
