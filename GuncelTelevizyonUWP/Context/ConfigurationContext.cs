9d43a44464683e014ac9896c8daaa078b4dc26eb
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-10 23:32:03 +0200

    New LocalFolder implementation and Azure container base url for channel images.

--- GuncelTelevizyonUWP/Context/ConfigurationContext.cs
+++ GuncelTelevizyonUWP/Context/ConfigurationContext.cs
@@ -4,11 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml;
 
 namespace GuncelTelevizyonUWP.Context
 {
     public static class ConfigurationContext
     {
+        public static string ChannelImageStorageURL = "https://gunceltelevizyon.blob.core.windows.net/channels/";
+        public static StorageFolder LocalFolder = ApplicationData.Current.LocalFolder;
+
         private static Settings _mainSettings;
         public static Settings MainSettings
         {
