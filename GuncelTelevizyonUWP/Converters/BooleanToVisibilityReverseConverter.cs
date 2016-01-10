3edc9d092cfc1ba3a0709b8d3db4be173f41a548
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-10 22:07:52 +0200

    New converters and it's resources are added.

---
+++ GuncelTelevizyonUWP/Converters/BooleanToVisibilityReverseConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace GuncelTelevizyonUWP.Converters
+{
+    public class BooleanToVisibilityReverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return false;
+
+            var sent = (bool)value;
+            return sent ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
