071c896a076afbb718e7e5e3e91ec714e1ff8f6a
Burak Kaan Köse <bkaankose@outlook.com>
2016-01-10 23:30:52 +0200

    SynchronizationService and it's interface implemented.
    This service will synchronize current active channel images with the
    device.Keeping images in the device will save a lot of mobile data and
    loading time.

---
+++ GuncelTelevizyonUWP/Interfaces/ISynchronizationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuncelTelevizyonUWP.Interfaces
+{
+    public interface ISynchronizationService
+    {
+        Task SynchronizeChannelImage(int channelId);
+        event EventHandler<int> ProgressChanged;
+    }
+}
---
+++ GuncelTelevizyonUWP/Services/SynchronizationService.cs
@@ -0,0 +1,43 @@
+﻿using GuncelTelevizyonUWP.Context;
+using GuncelTelevizyonUWP.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Networking.BackgroundTransfer;
+using Windows.Storage;
+
+namespace GuncelTelevizyonUWP.Services
+{
+    public class SynchronizationService : ISynchronizationService
+    {
+        private BackgroundDownloader _downloader = new BackgroundDownloader();
+        public SynchronizationService()
+        {
+            
+        }
+
+        public event EventHandler<int> ProgressChanged;
+
+        public async Task SynchronizeChannelImage(int channelId)
+        {
+            var imageExists = await ConfigurationContext.LocalFolder.TryGetItemAsync(string.Format("Images\\{0}.png", channelId));
+            if (imageExists == null)
+            { // Channel image is not stored yet , download and save.
+                IStorageFile downloadedImage = await ConfigurationContext.LocalFolder.CreateFileAsync(string.Format("Images\\{0}.png", channelId));
+                var operation = _downloader.CreateDownload(new Uri(string.Format("{0}{1}.png", ConfigurationContext.ChannelImageStorageURL, channelId), UriKind.Absolute), downloadedImage);
+                Progress<DownloadOperation> progress = new Progress<DownloadOperation>(progressChanged);
+                await operation.StartAsync().AsTask(new CancellationToken(), progress);
+            }
+        }
+
+        private void progressChanged(DownloadOperation downloadOperation)
+        {
+            int progress = (int)(100 * ((double)downloadOperation.Progress.BytesReceived / (double)downloadOperation.Progress.TotalBytesToReceive));
+            ProgressChanged.Invoke(this, progress);
+        }
+    }
+}
