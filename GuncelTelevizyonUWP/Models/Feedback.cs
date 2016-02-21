2b841874df461343ac39af63a6b66e872d246c08
Burak Kaan Köse <bkaankose@outlook.com>
2016-02-21 22:08:11 +0200

    Feedback model updated.

--- GuncelTelevizyonUWP/Models/Feedback.cs
+++ GuncelTelevizyonUWP/Models/Feedback.cs
@@ -9,14 +9,14 @@ namespace GuncelTelevizyonUWP.Models
 {
     public class Feedback
     {
-        public string Id { get; set; }
+        public string id { get; set; }
         [JsonProperty(PropertyName = "firstname")]
         public string FirstName { get; set; }
         [JsonProperty(PropertyName = "lastname")]
         public string LastName { get; set; }
         [JsonProperty(PropertyName = "email")]
         public string EMail { get; set; }
-        [JsonProperty(PropertyName = "message")]
-        public string Message { get; set; }
+        [JsonProperty(PropertyName = "body")]
+        public string Body { get; set; }
     }
 }
