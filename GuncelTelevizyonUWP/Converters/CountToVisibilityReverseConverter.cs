92d4f450af6ec65e3c778895f4e4914d66246348
Burak Kaan Köse <bkaankose@outlook.com>
2016-02-21 22:46:57 +0200

    New converters and style added.

---
+++ GuncelTelevizyonUWP/Converters/CountToVisibilityReverseConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace GuncelTelevizyonUWP.Converters
+{
+    public class CountToVisibilityReverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return Visibility.Visible;
+
+            var sent = (int)value;
+            return sent == 0 ? Visibility.Visible : Visibility.Collapsed;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
