9388fd9541a1edd7bd91a49ed60ff85aabb9ccd0
Burak Kaan Köse <bkaankose@outlook.com>
2016-02-21 22:32:53 +0200

    New DateTime to Timespan converter added.

---
+++ GuncelTelevizyonUWP/Converters/DateTimeToTimeSpanConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace GuncelTelevizyonUWP.Converters
+{
+    public class DateTimeToTimeSpanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return "(Bilinmiyor)";
+
+            var sent = (DateTime)value;
+            return sent.ToString("hh:mm");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
