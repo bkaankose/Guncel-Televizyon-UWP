ce1382f2b3a13cfd1840e2697d22d6724776cd29
Burak Kaan Köse <bkaankose@outlook.com>
2016-03-03 22:21:01 +0200

    New Quality model added.

---
+++ GuncelTelevizyonUWP/Models/QualityEnumModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuncelTelevizyonUWP.Models
+{
+    public enum QualityEnumModel
+    {
+        Düşük,
+        Orta,
+        Yüksek
+    }
+}
