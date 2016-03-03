348e49442964f2c974cd271d77b027878ed9fd6a
Burak Kaan Köse <bkaankose@outlook.com>
2016-03-03 22:25:30 +0200

    WatchPage is modified.
    * Bitrate support.
    * New TransportControls style.
    * MediaElement's source is changed to AdaptiveMediaSource in order to
    have Bandwith support.

--- GuncelTelevizyonUWP/ViewModels/WatchPageViewModel.cs
+++ GuncelTelevizyonUWP/ViewModels/WatchPageViewModel.cs
@@ -6,6 +6,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Media.Streaming.Adaptive;
+using Prism.Windows.Navigation;
+using GuncelTelevizyonUWP.Interfaces;
+using System.Collections.ObjectModel;
+using ByteSizeLib;
+using System.Diagnostics;
+using Newtonsoft.Json;
 
 namespace GuncelTelevizyonUWP.ViewModels
 {
@@ -13,6 +20,15 @@ namespace GuncelTelevizyonUWP.ViewModels
     {
 
         #region Properties
+        private IEnumerable<IGrouping<ChannelCategory, ChannelModelView>> _groupedChannels;
+
+        public IEnumerable<IGrouping<ChannelCategory, ChannelModelView>> GroupedChannels
+        {
+            get { return _groupedChannels; }
+            set { _groupedChannels = value; OnPropertyChanged("GroupedChannels"); }
+        }
+
+
 
         private bool _isChannelsMenuOpen;
 
@@ -23,40 +39,124 @@ namespace GuncelTelevizyonUWP.ViewModels
         }
 
 
-        private Uri _currentSource;
+        private AdaptiveMediaSource _currentSource;
 
-        public Uri CurrentSource
+        public AdaptiveMediaSource CurrentSource
         {
             get { return _currentSource; }
             set { _currentSource = value; OnPropertyChanged("CurrentSource"); }
         }
 
-        private Channel _currentChannel;
+        private ChannelModelView _currentChannel;
 
-        public Channel CurrentChannel
+        public ChannelModelView CurrentChannel
         {
             get { return _currentChannel; }
             set { _currentChannel = value; OnPropertyChanged("CurrentChannel"); }
         }
 
+        private ObservableCollection<ChannelModelView> _allChannels;
 
+        public ObservableCollection<ChannelModelView> AllChannels
+        {
+            get { return _allChannels; }
+            set { _allChannels = value; OnPropertyChanged("AllChannels"); }
+        }
+
+        private ObservableCollection<BitrateModelView> _availableBitrates;
+
+        public ObservableCollection<BitrateModelView> AvailableBitrates
+        {
+            get { return _availableBitrates; }
+            set { _availableBitrates = value;  }
+        }
+
+        private BitrateModelView _selectedBitrate;
+
+        public BitrateModelView SelectedBitrate
+        {
+            get { return _selectedBitrate; }
+            set { _selectedBitrate = value; OnPropertyChanged("SelectedBitrate"); }
+        }
 
         #endregion
 
         #region Commands
-
-        public DelegateCommand PaneClosingCommand { get; set; }
-        public DelegateCommand OpenPaneCommand { get; set; }
+        
 
         #endregion
-        public WatchPageViewModel()
+
+        #region Services & Repositories
+
+        private readonly INavigationService _navigationService;
+        private readonly IDialogService _dialogService;
+        private readonly IChannelRepository _channelRepository;
+        #endregion
+        public WatchPageViewModel(INavigationService navigationService,IDialogService dialogService,IChannelRepository channelRepository)
         {
-            CurrentSource = new Uri("http://mn-l.mncdn.com/showtv/showtv2/playlist.m3u8", UriKind.Absolute);
-            PaneClosingCommand = new DelegateCommand(new Action(() => IsChannelsMenuOpen = false));
-            OpenPaneCommand = new DelegateCommand(new Action(() =>
+            _navigationService = navigationService;
+            _dialogService = dialogService;
+            _channelRepository = channelRepository;
+        }
+
+        public override async void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+        {
+            base.OnNavigatedTo(e, viewModelState);
+
+            this.PropertyChanged += PropChanged;
+            AllChannels = await OperateAsyncOperation(_channelRepository.GetChannels(ChannelCategory.Hepsi));
+
+            CurrentChannel = AllChannels.FirstOrDefault(a => a.Channel.Id == (JsonConvert.DeserializeObject<ChannelModelView>(e.Parameter.ToString())).Channel.Id);
+        }
+
+        private async void PropChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(CurrentChannel))
             {
-                IsChannelsMenuOpen = true;
-            }));
+                if(!CurrentChannel.Channel.IsSpecial) // Normal channel
+                    OperateMediaStreamResult(await AdaptiveMediaSource.CreateFromUriAsync(new Uri(CurrentChannel.Channel.URL, UriKind.RelativeOrAbsolute)));
+                else // Special channel, request for a valid stream url.
+                {
+
+                }
+            }
+            else if (e.PropertyName == nameof(CurrentSource) && CurrentSource != null)
+            {
+                AvailableBitrates = new ObservableCollection<BitrateModelView>();
+                foreach (var bitrate in CurrentSource.AvailableBitrates.Where(a => a != 0))
+                    AvailableBitrates.Add(new BitrateModelView() { Bitrate = bitrate });
+
+                OnPropertyChanged("AvailableBitrates");
+                if(CurrentSettings.PreferedQuality == QualityEnumModel.Düşük)
+                    SelectedBitrate = AvailableBitrates.OrderBy(b => b.Bitrate).FirstOrDefault(a => a.Bitrate == CurrentSource.CurrentPlaybackBitrate);
+                else if(CurrentSettings.PreferedQuality == QualityEnumModel.Yüksek)
+                    SelectedBitrate = AvailableBitrates.OrderByDescending(b => b.Bitrate).FirstOrDefault(a => a.Bitrate == CurrentSource.CurrentPlaybackBitrate);
+                else if(CurrentSettings.PreferedQuality == QualityEnumModel.Orta)
+                    SelectedBitrate = AvailableBitrates.OrderBy(b => b.Bitrate).FirstOrDefault(a => a.Bitrate == AvailableBitrates[AvailableBitrates.Count / 2].Bitrate);
+            }
+            else if (e.PropertyName == nameof(SelectedBitrate) && SelectedBitrate != null && CurrentSource != null && SelectedBitrate.Bitrate != CurrentSource.CurrentPlaybackBitrate)
+                CurrentSource.DesiredMinBitrate = SelectedBitrate.Bitrate;
+            else if (e.PropertyName == nameof(AllChannels))
+                GroupedChannels = AllChannels.OrderBy(b => b.Channel.Name).GroupBy(a => a.Channel.Category);
+        }
+
+        private async void OperateMediaStreamResult(AdaptiveMediaSourceCreationResult result)
+        {
+            switch(result.Status)
+            {
+                case AdaptiveMediaSourceCreationStatus.Success:
+                    CurrentSource = result.MediaSource;
+                    CurrentSource.DownloadBitrateChanged += (c, r) => { Debug.WriteLine("Download : {0}", CurrentSource.CurrentDownloadBitrate); };
+                    CurrentSource.PlaybackBitrateChanged += (c, r) => { Debug.WriteLine("Playback : {0}", CurrentSource.CurrentPlaybackBitrate); };
+                    break;
+                default:
+                    var reportResult = await _dialogService.ShowConfirmationDialogAsync(string.Format("Show TV yüklenemedi."), ":(");
+                    if(reportResult)
+                    {
+                        // TODO : Report channel
+                    }
+                    break;
+            }
         }
     }
 }
--- GuncelTelevizyonUWP/Views/WatchPage.xaml.cs
+++ GuncelTelevizyonUWP/Views/WatchPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using GuncelTelevizyonUWP.Helpers;
+using GuncelTelevizyonUWP.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -19,9 +20,44 @@ namespace GuncelTelevizyonUWP.Views
 {
     public sealed partial class WatchPage : BasePage
     {
+        private WatchPageViewModel ViewModel;
         public WatchPage()
         {
             this.InitializeComponent();
+            ViewModel = this.DataContext as WatchPageViewModel;
+            ViewModel.PropertyChanged += PropChanged;
+        }
+
+        private void PropChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "CurrentSource" && ViewModel.CurrentSource != null)
+                mediaElement.SetMediaStreamSource(ViewModel.CurrentSource);
+        }
+
+        private void SplitView_PaneClosed(SplitView sender, object args)
+        {
+            ViewModel.IsChannelsMenuOpen = false;
+        }
+
+        private void BitrateChangedEvent(object sender, SelectionChangedEventArgs e)
+        {
+            HideAppBarFlyout("BitrateAppBarButton");
+        }
+
+        private void DisposeFlyouts()
+        {
+            HideAppBarFlyout("BitrateAppBarButton");
+            HideAppBarFlyout("ChannelsAppBarButton");
+        }
+
+        private void HideAppBarFlyout(string AppBarButtonName)
+        {
+            (ChildTemplateControlHelper.FindChildControl<AppBarButton>(mediaElement.TransportControls, AppBarButtonName) as AppBarButton).Flyout.Hide();
+        }
+
+        private void ChannelChangedEvent(object sender, SelectionChangedEventArgs e)
+        {
+            HideAppBarFlyout("ChannelsAppBarButton");
         }
     }
 }
