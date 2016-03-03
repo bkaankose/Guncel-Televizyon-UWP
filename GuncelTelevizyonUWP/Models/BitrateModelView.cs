c7bcd510658a8d5188a638e973f191e57297b50d
Burak Kaan Köse <bkaankose@outlook.com>
2016-03-03 22:23:40 +0200

    BitrateModelView added.

---
+++ GuncelTelevizyonUWP/Models/BitrateModelView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuncelTelevizyonUWP.Models
+{
+    public class BitrateModelView
+    {
+        public uint Bitrate { get; set; }
+    }
+}
