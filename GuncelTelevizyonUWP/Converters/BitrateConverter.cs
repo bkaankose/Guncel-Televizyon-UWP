043d1ae1eefb7d20ee3fa1c01b00a741851b74ae
Burak Kaan Köse <bkaankose@outlook.com>
2016-03-03 22:25:58 +0200

    BitrateConverter implemented in order to be used in WatchPage UI.

---
+++ GuncelTelevizyonUWP/Converters/BitrateConverter.cs
@@ -0,0 +1,24 @@
+﻿using ByteSizeLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace GuncelTelevizyonUWP.Converters
+{
+    public class BitrateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var sent = (uint)value;
+            return string.Format("{0} kbps", ByteSize.FromBits(sent).KiloBytes.ToString().Split('.')[0]);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
